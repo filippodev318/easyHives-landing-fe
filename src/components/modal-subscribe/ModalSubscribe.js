5cfd748b2d6ec485da9eceb181a31ad90864cf42
filippoperna <stintaf22GitHub>
2023-03-02 21:06:23 +0100

    aggiunto logo

--- src/components/modal-subscribe/ModalSubscribe.js
+++ src/components/modal-subscribe/ModalSubscribe.js
@@ -10,19 +10,21 @@ const ModalSubscribe = ({
     }
 
     return (
-        <div className='easyHives__modal-subscribe'>
-            <div className='easyHives__modal-subscribe-content'>
-                <div className='easyHives__modal-subscribe-content-header-close'>
-                    <button onClick={handleClose}><CloseIcon /></button>
-                </div>
+        <div className='easyHives__modal-subscribe-container'>
+            <div className='easyHives__modal-subscribe'>
+                <div className='easyHives__modal-subscribe-content'>
+                    <div className='easyHives__modal-subscribe-content-header-close'>
+                        <button onClick={handleClose}><CloseIcon /></button>
+                    </div>
 
-                <div className='easyHives__modal-subscribe-content-header'>
-                    <h1 className='text__light font-Recoleta-Bold'>Resta aggiornato</h1>
-                    <p>sii tra i primi a provare l'app questa estate!</p>
-                </div>
-                <div className='easyHives__modal-subscribe-content-body'>
-                    <Form submitData={submitData}
-                    />
+                    <div className='easyHives__modal-subscribe-content-header'>
+                        <h1 className='text__light font-Recoleta-Bold'>Resta aggiornato</h1>
+                        <p>sii tra i primi a provare l'app questa estate!</p>
+                    </div>
+                    <div className='easyHives__modal-subscribe-content-body'>
+                        <Form submitData={submitData}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
